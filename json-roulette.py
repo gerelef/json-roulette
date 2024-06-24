e102988ff4ca831fdd583926eac2075c9ceb8013
gerelef <44060598+gerelef@users.noreply.github.com>
2024-06-24 17:58:06 +0300

    added jarray generation; added nullable flag

--- json-roulette.py
+++ json-roulette.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3.12
 import argparse
+import functools
 import json
 import os
 import random
 import string
 import sys
+import time
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable
@@ -32,60 +34,58 @@ def field_roulette(json: dict) -> bool:
     return edit_field
 
 
-def generate_random_string(nullable=False) -> str | None:
-    # 1 in 10 chance return null
-    if nullable and decide(0.1):
+def generate_random_string(nullability_chance) -> str | None:
+    if decide(nullability_chance):
         return None
-    return "".join(random.choices(string.ascii_letters, k=random.randint(1, 5)))
+    return "".join(random.choices(string.ascii_letters, k=random.randint(3, 9)))
 
 
-def generate_random_int(nullable=False) -> int | None:
-    # 1 in 10 chance return null
-    if nullable and decide(0.1):
+def generate_random_int(nullability_chance) -> int | None:
+    if decide(nullability_chance):
         return None
     return random.randint(-999, 10000)
 
 
-def generate_random_double(nullable=False) -> float | None:
-    # 1 in 10 chance return null
-    if nullable and decide(0.1):
+def generate_random_double(nullability_chance) -> float | None:
+    if decide(nullability_chance):
         return None
     return random.uniform(-1000, 1000)
 
 
-def generate_random_bool(nullable=False) -> bool | None:
-    # 1 in 10 chance return null
-    if nullable and decide(0.1):
+def generate_random_bool(nullability_chance) -> bool | None:
+    if decide(nullability_chance):
         return None
     return decide(random.uniform(0.2, 0.8))
 
 
-def generate_random_jfield(*primitive_generators: Callable[[], object]) -> tuple[str, ...]:
-    return random.choice(words), random.choice(primitive_generators)()
+def generate_random_jfield(*generators: Callable[[], object], **kwargs) -> tuple[str, ...]:
+    return random.choice(words), random.choice(generators)(**kwargs)
 
 
 def generate_jobj(
+        primitive_generator: Callable,
+        composite_generator: Callable,
         length_low: int,
         length_high: int,
         nested_chance: float,
         nested_max_depth: int,
-        current_depth: int = 0) -> dict:
+        current_depth: int,
+        nullability_chance: float) -> dict | None:
+    if decide(nullability_chance):
+        return None
+
     out = {}
     jobj_length = random.randint(length_low, length_high)
     for i in range(jobj_length):
-        key, value = generate_random_jfield(
-            generate_random_string,
-            generate_random_int,
-            generate_random_double,
-            generate_random_bool
-        )
+        key, value = primitive_generator()
         if current_depth < nested_max_depth and decide(nested_chance):
-            key = random.choice(words)
-            value = generate_jobj(
-                length_low,
-                length_high,
-                nested_chance,
-                nested_max_depth,
+            key, value = composite_generator(
+                primitive_generator=primitive_generator,
+                composite_generator=composite_generator,
+                length_low=length_low,
+                length_high=length_high,
+                nested_chance=nested_chance,
+                nested_max_depth=nested_max_depth,
                 current_depth=current_depth + 1,
             )
         out[key] = value
@@ -93,12 +93,32 @@ def generate_jobj(
 
 
 def generate_jarr(
+        primitive_generator: Callable,
+        composite_generator: Callable,
         length_low: int,
         length_high: int,
         nested_chance: float,
         nested_max_depth: int,
-        current_depth: int = 0) -> dict:
-    raise NotImplementedError()
+        current_depth: int,
+        nullability_chance: float) -> list | None:
+    if decide(nullability_chance):
+        return None
+    out = []
+    jobj_length = random.randint(length_low, length_high)
+    for i in range(jobj_length):
+        _, value = primitive_generator()
+        if current_depth < nested_max_depth and decide(nested_chance):
+            _, value = composite_generator(
+                primitive_generator=primitive_generator,
+                composite_generator=composite_generator,
+                length_low=length_low,
+                length_high=length_high,
+                nested_chance=nested_chance,
+                nested_max_depth=nested_max_depth,
+                current_depth=current_depth + 1,
+            )
+        out.append(value)
+    return out
 
 
 @dataclass
@@ -123,15 +143,16 @@ class UserOptions:
     pretty: bool
     # --seed
     seed: int
+    # --nullable <float> <= 1 (negative to never be null)
+    nullable_chance: float
 
 
 def _parse_args(args) -> UserOptions:
     parser = argparse.ArgumentParser(description="json-roulette: a barebones json generator, for testing")
     parser.add_argument("--size", type=int, required=True)
-    # group = parser.add_mutually_exclusive_group(required=True)
-    # disabled until further notice
-    # group.add_argument("--objects", default=False, action="store_true")
-    # group.add_argument("--arrays", default=False, action="store_true")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--objects", default=False, action="store_true")
+    group.add_argument("--arrays", default=False, action="store_true")
     parser.add_argument("--composites-size-low", type=int, required=True)
     parser.add_argument("--composites-size-high", type=int, required=True)
     # optionals
@@ -140,9 +161,10 @@ def _parse_args(args) -> UserOptions:
     nested_flags_group = parser.add_mutually_exclusive_group(required=False)
     nested_flags_group.add_argument("--flat", default=False, action="store_true", required=False)
     nested_flags_group.add_argument("--nested-chance", type=float, default=0.2, required=False)
-    parser.add_argument("--nested-max-depth", type=int, default=9999, required=False)
+    parser.add_argument("--nested-max-depth", type=int, default=sys.getrecursionlimit() // 2, required=False)
     parser.add_argument("--pretty", default=False, action="store_true", required=False)
-    parser.add_argument("--seed", type=int, default=1337, required=False)
+    parser.add_argument("--seed", default=time.time(), required=False)
+    parser.add_argument("--nullable-chance", type=float, default=0.05, required=False)
     options = parser.parse_args(args)
     local_words = "resources/words"
     tempfile = "/tmp/tempfile_words"
@@ -152,16 +174,18 @@ def _parse_args(args) -> UserOptions:
     if not options.word_file and not local_words_exists:
         import urllib.request
         with open(tempfile, "wb") as word_file:
-            word_file.write(urllib.request.urlopen("https://raw.githubusercontent.com/gerelef/json-roulette/master/resources/words").read())
+            word_file.write(
+                urllib.request.urlopen(
+                    "https://raw.githubusercontent.com/gerelef/json-roulette/master/resources/words"
+                ).read()
+            )
     assert 1 <= options.size
     assert 1 <= options.composites_size_low <= options.composites_size_high
     assert 1 <= options.word_sample_size
     assert 0.0 <= options.nested_chance <= 1.0
     assert 1 <= options.nested_max_depth
     return UserOptions(
-        output_is_jobject_else_array=True,
-        # disabled until further notice
-        # output_is_jobject_else_array=options.objects,
+        output_is_jobject_else_array=options.objects,
         output_size=options.size,
         composites_size_low=options.composites_size_low,
         composites_size_high=options.composites_size_high,
@@ -170,13 +194,13 @@ def _parse_args(args) -> UserOptions:
         nested_chance=-1 if options.flat else options.nested_chance,
         nested_max_depth=1 if options.flat else options.nested_max_depth,
         pretty=options.pretty,
-        seed=options.seed
+        seed=options.seed,
+        nullable_chance=options.nullable_chance
     )
 
 
 words = []
 if __name__ == "__main__":
-    sys.setrecursionlimit(100_000)
     options = _parse_args(sys.argv[1:])
     with open(options.path_to_word_file, "r") as dictionary:
         for line in dictionary:
@@ -184,17 +208,39 @@ if __name__ == "__main__":
         words = tuple(sorted(random.choices(words, k=options.word_sample_size)))
 
     random.seed(options.seed)
+    primitive_generator = functools.partial(
+        generate_random_jfield,
+        generate_random_string,
+        generate_random_int,
+        generate_random_double,
+        generate_random_bool,
+        nullability_chance=options.nullable_chance
+    )
+    composite_generator = functools.partial(
+        generate_random_jfield,
+        generate_jobj,
+        generate_jarr,
+        nullability_chance=options.nullable_chance
+    )
     for i in range(options.output_size):
         generated = generate_jobj(
-            options.composites_size_low,
-            options.composites_size_high,
-            options.nested_chance,
-            options.nested_max_depth
+            primitive_generator,
+            composite_generator,
+            length_low=options.composites_size_low,
+            length_high=options.composites_size_high,
+            nested_chance=options.nested_chance,
+            nested_max_depth=options.nested_max_depth,
+            current_depth=1,
+            nullability_chance=-1
         ) if options.output_is_jobject_else_array else generate_jarr(
-            options.composites_size_low,
-            options.composites_size_high,
-            options.nested_chance,
-            options.nested_max_depth
+            primitive_generator,
+            composite_generator,
+            length_low=options.composites_size_low,
+            length_high=options.composites_size_high,
+            nested_chance=options.nested_chance,
+            nested_max_depth=options.nested_max_depth,
+            current_depth=1,
+            nullability_chance=-1
         )
         print(json.dumps(generated, indent=4 if options.pretty else None))
 
